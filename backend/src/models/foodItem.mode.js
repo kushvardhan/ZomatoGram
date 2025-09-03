19d164b13254d97ff014f6554cba60681f84c199
Kush Vardhan <kushvardhan39797@gmail.com>
2025-09-04 00:21:18 +0530

    .

--- backend/src/models/foodItem.mode.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const foodItems = new mongoose.Schema({
-
-});
-
-const foodItemsModel = mongoose.model("footitems", foodItems);
-
-module.exports=foodItemsModel;
