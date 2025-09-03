fa520a43206ae711094c5bba265e2cb874a7bec0
Kush Vardhan <kushvardhan39797@gmail.com>
2025-09-03 20:44:35 +0530

    middleware and package import

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
+const cookieParser = require('cookie-parser');
+
+const authRouter = require("./routes/auth.routes");
+
+app.use(express.json());
+app.use(cookieParser());
 
 app.get('/',(req,res)=>{
     res.send('he');
 })
+
+app.use('/api/auth', authRouter);
 
 module.exports=app;
