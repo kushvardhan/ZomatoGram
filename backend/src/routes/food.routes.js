20454a125ec37697f3d8b70c6d682ca4b8c73c1d
Kush Vardhan <kushvardhan39797@gmail.com>
2025-09-04 19:21:49 +0530

    checking incoming data in food.routes, installed ImageKit

--- backend/src/routes/food.routes.js
+++ backend/src/routes/food.routes.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 const {createFood} = require("../controllers/food.controller");
 const {authFoodPartnerMiddleware} = require("../middleware/auth.middleware");
+const multer = require("multer");
 
-router.post("/", authFoodPartnerMiddleware, createFood);
+const upload = multer({
+    storage: multer.memoryStorage(),
+})
+
+router.post("/", authFoodPartnerMiddleware, upload.single("video"), createFood);
 
 module.exports=router;
